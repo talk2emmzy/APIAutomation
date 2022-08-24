75b7a9aeab46f11ffa41cd5a6531eeacaf9357ae
Okoye Emmanuel <Okoye@omnibiz.com>
2022-08-24 18:41:22 +0100

    New Test

---
+++ src/test/java/TestNG_01.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+
+public class TestNG_01 {
+
+    @Test
+    void Test_01(){
+
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody().asString());
+        System.out.println(response.asString());
+        System.out.println(response.getStatusLine());
+        System.out.println(response.getHeader("content-type"));
+        System.out.println(response.getTime());
+
+
+
+    }
+}
